b7fd534b7547fe209a3e4252ee34f23d00221cdd
Avenir94 <realavenir@gmail.com>
2024-07-05 13:20:29 -0400

    lots made
    changes made to stuff

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,7 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Injectable } from '@angular/core';
 import { ApiserviceService } from './apiservice.service';
+import { Observable } from 'rxjs';
+import { DomSanitizer } from '@angular/platform-browser'
+import { PipeTransform, Pipe } from "@angular/core";
 
 @Component({
   selector: 'app-root',
@@ -10,15 +14,42 @@ import { ApiserviceService } from './apiservice.service';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
+@Injectable({providedIn:'root'})
 export class AppComponent {
   title = 'angular'
-  constructor (private apiService: ApiserviceService){}
-  clickMe(event: any) {
-    console.log(event);
-    console.log(event.target);
-    console.log(event.target.id);
-    this.title = "Hello";
+  income: string = ' ';
+  constructor (private apiService: ApiserviceService, private http: HttpClient){}
+  onclick(event: any) {
+    let req = this.http.get('https://api.worldbank.org/v2/country/${event.country}/?format=json');
+    console.log(req.subscribe());
   }
-  
-  ;
+
 }
+
+
+
+
+
+
+
+// private loadDoc(country : string) {
+//   var xhr = new XMLHttpRequest();
+//   xhr.onload = function() {
+//   let obj = JSON.parse(this.responseText);
+//   let income = `Income Level: ${obj[1][0].incomeLevel.value}`;
+//   let regionName = `Region Name: ${obj[1][0].region.value}`;
+//   let capitalCity = `Capital City: ${obj[1][0].capitalCity}`;
+//   let countryName = `Country Name: ${obj[1][0].name}`;
+//   let regionId = `Region ID: ${obj[1][0].region.id}`;
+//   let countryCode = `Country Code: ${obj[1][0].iso2Code}`;
+//   let theString = `Country Name: ${countryName}, Income Level: ${income}`;
+//   }
+//   xhr.open("GET", `https://api.worldbank.org/v2/country/${country}/?format=json`);
+//   xhr.send();
+//   return xhr.responseText;
+// }
+
+
+
+
